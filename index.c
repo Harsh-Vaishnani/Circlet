2b5f3326bc276e83219bc6b8b72ebb3caf1b02a4
Harsh-Vaishani <harshvaishnani8@gmail.com>
2024-01-04 22:12:57 +0530

    My project

---
+++ index.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+int main()
+{
+
+                                        // Question-1
+
+    for (int i = 1; i <= 7; i++)
+    {
+        printf(" * ");
+    }
+    printf("\n");
+    for (int i = 1; i <= 7; i++)
+    {
+        printf(" * ");
+    }
+    printf("\n");
+    for (int i = 1; i <= 7; i++)
+    {
+        printf(" * ");
+    }
+    printf("\n");
+    for (int i = 1; i <= 7; i++)
+    {
+        printf(" * ");
+    }
+    printf("\n");
+
+                                           // Question-2
+
+    // int main()
+    // {
+    //     int num[36];
+    //     int even = 0;
+    //     int odd = 0;
+    //     for (int i = 0; i <= 4; i++)
+    //     {
+    //         scanf("%d\n", &num[i]);
+    //         if (i % 2 == 0)
+    //         {
+    //             // printf("Even Number");
+    //             even++;
+    //         }
+    //         else
+    //         {
+    //             odd++;
+    //         }
+    //     }
+    //     printf("This is Even Number ");
+    //     printf("%d\n", even);
+    //     printf("This is Odd Number ");
+    //     printf("%d\n", odd);
+
+        return 0;
+    }
